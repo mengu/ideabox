29c5bfb395deaad690b40fe275cd9b56a05bcef3
mengu <mengukagan@gmail.com>
2010-06-28 04:41:00 +0300

    changed project display layout, added icons to display if a task is completed or not, added checkboxes to work with ajax to mark a task as completed or not completed.

--- ideabox/public/js/main.js
+++ ideabox/public/js/main.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
-
+    $("input[type='checkbox']").click(function(){
+       var completed = $(this).attr('checked') ? true : false;
+       var completedImage = completed ? '<img src="../../img/check.png" border="0" />' : '<img src="../../img/undone.png" border="0" />';
+       $.post('/tasks/complete/'+this.id, {"completed": completed});
+       $("#completed_"+this.id).html(completedImage);
+    });
 });
 
