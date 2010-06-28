c82564041794d57c350b2d24329b2888c63e48cb
mengu <mengukagan@gmail.com>
2010-06-28 04:38:17 +0300

    updated complete action, added login check.

--- ideabox/controllers/tasks.py
+++ ideabox/controllers/tasks.py
@@ -4,6 +4,7 @@ from pylons import request, response, session, tmpl_context as c, url
 from pylons.controllers.util import abort, redirect
 
 from ideabox.lib.base import BaseController, Session, render
+from ideabox.lib.helpers import did_login_or_redirect
 from ideabox.model.project import Task
 from ideabox.model.user import User
 from datetime import datetime
@@ -13,14 +14,14 @@ log = logging.getLogger(__name__)
 class TasksController(BaseController):
 
     def new(self):
-#        did_login_or_redirect(session)
+        did_login_or_redirect(session)
         if not request.params["projectid"]:
             return redirect("/projects")
         users = Session.query(User).all()
         return render("tasks/new.html", {"users": users, "projectid":request.params["projectid"]})
 
     def create(self):
-#        did_login_or_redirect(session)
+        did_login_or_redirect(session)
         task_dict = {}
         for param in request.params:
             task_dict[param] = request.params[param]
@@ -36,24 +37,24 @@ class TasksController(BaseController):
             assigned_user = Session.query(User).filter_by(id=task.assigned_to).one()
         except:
             abort(404)
-        print task.status
         return render("tasks/show.html", {"task": task, "assigned_user":assigned_user})
 
     def complete(self, id):
-#        did_login_or_redirect(session)
+        did_login_or_redirect(session)
         try:
             task = Session.query(Task).filter_by(id=id).one()
         except:
             abort(404)
-        mark_completed = Session.query(Task).filter_by(id=id).update({
-            "status": True,
-            "completed_at": datetime.now()
-            })
+        completed = True if request.params["completed"] == "true" else False
+        completed_at = datetime.now() if completed else None
+        change_status = Session.query(Task).filter_by(id=id).update({
+            "completed": completed,
+            "completed_at": completed_at
+        })
         Session.commit()
-        return redirect("/tasks/show/%s" % task.id)
 
     def delete(self, id):
-#        did_login_or_redirect(session)
+        did_login_or_redirect(session)
         try:
             task = Session.query(Task).filter_by(id=id).delete()
         except:
