6f02ae572897418bee431d801069de187458b48f
mengu <mengukagan@gmail.com>
2010-07-10 03:13:53 +0300

    removed did_login_or_redirect method. Added a note for the slugify method.

--- ideabox/lib/helpers.py
+++ ideabox/lib/helpers.py
@@ -6,19 +6,12 @@ available to Controllers. This module is available to templates as 'h'.
 # Import helpers as desired, or define your own, ie:
 #from webhelpers.html.tags import checkbox, password
 from pylons.controllers.util import redirect
-def did_login_or_redirect(session):
-    print session
-    if "user" not in session:
-        return redirect("/users/login")
-    else:
-        if not session["user"]["id"] > 0:
-            return redirect("/users/login")
-
 
 def slugify(value):
     """
     Normalizes string, converts to lowercase, removes non-alpha characters,
     and converts spaces to hyphens.
+    Borrowed from Django.
     """
     import unicodedata
     import re
