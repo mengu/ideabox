6ff25eccf7d74957f6e00ba5d95630b30e915dc3
mengu <mengukagan@gmail.com>
2010-07-11 15:52:32 +0300

    updated for auth control.

--- ideabox/controllers/notes.py
+++ ideabox/controllers/notes.py
@@ -1,8 +1,5 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
 
-__author__="mengu"
-__date__ ="$Jul 1, 2010 1:34:28 AM$"
+
 
 import logging
 
@@ -10,7 +7,6 @@ from pylons import request, response, session, tmpl_context as c, url
 from pylons.controllers.util import abort, redirect
 
 from ideabox.lib.base import BaseController, Session, render
-from ideabox.lib.helpers import did_login_or_redirect
 from ideabox.model.project import Note
 from ideabox.model.user import User
 from datetime import datetime
@@ -19,8 +15,12 @@ log = logging.getLogger(__name__)
 
 class NotesController(BaseController):
 
+    def __before__(self, action, **params):
+        filter_actions = ["new", "edit", "create", "update", "delete"]
+        if "user" not in session and action in filter_actions:
+            redirect("/users/login")
+
     def create(self):
-        did_login_or_redirect(session)
         note_dict = {}
         for param in request.params:
             note_dict[param] = request.params[param]
@@ -31,9 +31,8 @@ class NotesController(BaseController):
         return redirect("/tasks/show/%s" % note_dict["task"])
 
     def delete(self, id):
-        did_login_or_redirect(session)
         try:
-            Note = Session.query(Note).filter_by(id=id).delete()
+            note = Session.query(Note).filter_by(id=id).delete()
         except:
             abort(404)
         return redirect("/Notes")
