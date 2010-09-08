2c8bdbb0c6b5914346987489de95f7922839d644
mengu <mengukagan@gmail.com>
2010-09-09 00:44:48 +0300

    updated the input check control, added new functions for auto complete.

--- ideabox/public/js/main.js
+++ ideabox/public/js/main.js
@@ -5,6 +5,27 @@ $(document).ready(function(){
        $.post('/tasks/complete/'+this.id, {"completed": completed});
        $("#completed_"+this.id).html(completedImage);
     });
-    $("input").attr("checked").css("background", "#000");
+
+    if (($("input").size > 0) && ($("input").attr("checked").size > 0)) {
+        $("input").attr("checked").css("background", "#000");
+    }
+
+    // auto complete
+    if (jQuery.isFunction(jQuery.fn.autocomplete)) {
+        $("#username-box").autocomplete("/users/find");
+    }
+    // add user
+    $("#add-user").click(function(){
+        var project_id = $("#project_id").val();
+        var username = $("#username-box").val();
+        $.post("/projects/newuser/"+project_id, {"username": username}, function(data){
+            $(".user-list").append('<div class="line">\
+              <div class="unit size1of5"><a href="/users/show/'+data.id+'">'+data.firstname+' '+data.lastname+'</a></div>\
+              <div class="unit size1of3"><a href="/projects/deleteuser/'+data.id+'">Delete</a></div>\
+          </div>');
+        });
+        return false;
+    });
+
 });
 
